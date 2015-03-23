1f4dd43e3acc1d5892a58e4b822de51c4f9634c1
Elias Luiz <eliasluizjr@gmail.com>
2015-03-23 20:21:13 -0300

    Ignore

--- AnalisadorLexico/AnalisadorLexico.java
+++ AnalisadorLexico/AnalisadorLexico.java
@@ -11,14 +11,14 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 public class AnalisadorLexico {
-    
+
     private LinkedHashMap<Integer, ArrayList<String>> tokens;
     private HashMap<String, String> lexemas;
-    
-    public AnalisadorLexico(){        
+
+    public AnalisadorLexico() {
         tokens = new LinkedHashMap<>();
         lexemas = new HashMap<>();
-        
+
         //############### LEXEMAS DA LINGUAGEM #################
         //Aritmeticos
         lexemas.put("+", "sum");
@@ -45,63 +45,111 @@ public class AnalisadorLexico {
         lexemas.put("str", "str,");
         lexemas.put("var", "id,");
         //Palavras-chave
-            //Condicionais
-            lexemas.put("se", "cond");
-            lexemas.put("então", "initcond");
-            lexemas.put("senão", "altcond");
-            lexemas.put("fim-se", "endcond");
-            //Loops
-            lexemas.put("para", "forloop");
-            lexemas.put("de", "rng1forloop");
-            lexemas.put("até", "rng2forloop");
-            lexemas.put("faça", "initforloop");
-            lexemas.put("fim-para", "endforloop");
-            lexemas.put("enquanto", "whileloop");
-            lexemas.put("fim-enquanto", "endwhileloop");
+        //Condicionais
+        lexemas.put("se", "cond");
+        lexemas.put("então", "initcond");
+        lexemas.put("senão", "altcond");
+        lexemas.put("fim-se", "endcond");
+        //Loops
+        lexemas.put("para", "forloop");
+        lexemas.put("de", "rng1forloop");
+        lexemas.put("até", "rng2forloop");
+        lexemas.put("faça", "initforloop");
+        lexemas.put("fim-para", "endforloop");
+        lexemas.put("enquanto", "whileloop");
+        lexemas.put("fim-enquanto", "endwhileloop");
         //#####################################################
-        
+
     }
-    
-    public void analisar(String path) throws IOException{
+
+    public void analisar(String path) throws IOException {
         BufferedReader b = carregar("exemplo.txt");
         String linha;
-        int nlinha = 0;
+
+        int nlinha = 1, index = 0;
         char c;
-        while(b.ready()){
+        
+        boolean isComment = false, isNum = false, isVar = false, isString = false;
+        int lexStart;
+
+        while (b.ready()) {
             linha = b.readLine();
-            
-            for(int i = 0; i < linha.length(); i++){
-                
+
+            System.out.print(nlinha + "  ");
+
+            for (int i = 0; i < linha.length(); i++) {
+
                 c = linha.charAt(i);
                 
+                /*
+                    MUDAR DE IF PARA SWITCH ?
+                */
+
+                //Identificacao e "pulo" dos comentarios
+                if (c == '#') {
+                    isComment = !isComment;
+                    continue;
+                }
+                if (isComment) {
+                    continue;
+                }
+
+                //Identificacao de strings
+                if (c == '"') {
+                    if (isString) {
+                        //gera token de string
+                        //linha[lexStart:i]
+                    } else {
+                        lexStart = i+1;
+                    }
+
+                    isString = !isString;
+                }
+
+                //Tratamento de espacos em branco
+                if ( ( c == ' ' ) ||
+                     ( i == linha.length() - 1 ) ) {
+                    if (isNum) {
+                        //gera token de numero
+                        //linha[lexStart:i]
+                        isNum = false;
+                    } else if (isVar) {
+                        //gera token de variavel
+                        //linha[lexStart:i]
+                        isVar = false;
+                    }
+                }
+                
+                
+
                 //TO DO
-                
-                
+                System.out.print(c);
+
             }
-            
+
+            System.out.println("");
+
             // TO DO
-            
-            
             nlinha++;
         }
     }
-    
-    private BufferedReader carregar(String path) throws IOException{
+
+    private BufferedReader carregar(String path) throws IOException {
         BufferedReader reader;
         reader = new BufferedReader(new InputStreamReader(
-                    new FileInputStream(path), "Cp1252"));
+                new FileInputStream(path), "Cp1252"));
         return reader;
     }
-    
-    public void salvar(String path) throws IOException{
+
+    public void salvar(String path) throws IOException {
         FileOutputStream fos = new FileOutputStream(path);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(tokens);
         oos.close();
         fos.close();
     }
-    
-    public static void main(String[] args) throws IOException, ClassNotFoundException{
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         AnalisadorLexico a = new AnalisadorLexico();
         a.analisar("exemplo.txt");
         a.salvar("tokens.txt");
